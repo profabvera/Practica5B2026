ceb872f7f8bcdf47c034c42feb6a557a512150cf
Armando B. VERA <armando@debian.profabvera>
2026-03-16 20:46:00 -0300

    Primer comit del Repo 2026

---
+++ misDatos.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Nombre-------------: Fulano" << endl;
+	cout << "Fecha de Nacimiento: 16/05/1998 " << endl;
+	cout << "Deporte Favorito---: Ajedrez" << endl;
+	return 0;
+}
